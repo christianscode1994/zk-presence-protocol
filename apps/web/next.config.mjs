5341c1be9216162c8554d83340b580b977203406
christianscode1994 <fourieranalys@hotmail.com>
2025-11-09 19:26:34 +0200

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
-    NEXT_PUBLIC_SUPPORT_EMAIL: process.env.NEXT_PUBLIC_SUPPORT_EMAIL
-  }
+  reactStrictMode: true
 };
 export default nextConfig;
